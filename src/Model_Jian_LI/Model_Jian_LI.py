07cab4b6e32226afbf9f1ff980a9c0f307f5ebda
afknst <k.afknst@gmail.com>
2021-10-30 09:50:21 +0200

    first_submission

---
+++ src/Model_Jian_LI/Model_Jian_LI.py
@@ -0,0 +1,29 @@
+import pickle
+import numpy as np
+
+
+def pload(_name):
+    with open(f"{_name}.p", "rb") as _f:
+        return pickle.load(_f)
+
+
+D = 4
+SIG, CLF, MU, N, RNG = pload('parameters')
+SIG = np.array(SIG)
+NOISE = MU * np.genfromtxt('noise.csv', delimiter=",")
+GUILD = "Lbitrik"
+
+
+def samples(clf=CLF, size=N, rng=RNG):
+    clf.set_params(random_state=rng)
+    rng = np.random.default_rng(rng)
+    _G, _ = clf.sample(size)
+    _G += NOISE
+    _G = np.maximum(_G, 1e-6)
+    _G = np.minimum(_G, 1 - 1e-6)
+    return np.sqrt(-2 * np.log(_G)) * SIG
+
+
+if __name__ == '__main__':
+    SAMPLES = samples()
+    np.savetxt(f"{GUILD}-first_submission.csv", SAMPLES, delimiter=",")
